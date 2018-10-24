7e903e6142f2a242021a4610dd0bf641c2b325b6
Tynise <35874024+Tynise@users.noreply.github.com>
2018-10-24 19:27:38 -0400

    Update src/components/FooterComponent.jsx
    did not make any changes as of yet

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 class Footer extends Component{
-
+    render(){
+        return(
+            <div> </div>
+        )
+    }
 }
 export default HomePage;
