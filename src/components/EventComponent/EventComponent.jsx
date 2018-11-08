9f8780d219e77d0c0dc2a4feaae86a6ed0fcfe82
kchan2 <kayingchan0903@ymail.com>
2018-11-07 19:30:20 -0500

    Image
    Images added

--- src/components/EventComponent/EventComponent.jsx
+++ src/components/EventComponent/EventComponent.jsx
@@ -6,13 +6,15 @@ class Event extends Component{
         return(
             <div>
                 <div className="title">Events</div>
-                <div className="flex-container">
+                <div className="flex-container2">
+                    <div className="image"><img src="IMG_1789.jpg" alt="Ada Lovelace Hackathon"></img></div>
+                    <div className="image"><img src="IMG_1563.jpg" alt="Women in Computing"></img></div>
+                    <div className="image"><img src="IMG_1730.jpg" alt="Memory"></img></div>
+                </div>
+                <div className="flex-container1">
                     <div className="textblock1">RSVP</div>
                     <div className="textblock2">RSVP</div>
-                    <div className="textblock3">Check this out</div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    <div className="textblock3">More...</div>
                 </div>
             </div>
         )
