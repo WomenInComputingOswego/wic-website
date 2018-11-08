60e2801bf7cf82e95d07c3b905a0b3a6b81d74a4
kchan2 <kayingchan0903@ymail.com>
2018-11-07 20:28:32 -0500

    Image Fixed
    Resized

--- src/components/EventComponent/EventComponent.jsx
+++ src/components/EventComponent/EventComponent.jsx
@@ -1,20 +1,20 @@
 import React, { Component } from "react";
 import './EventComponent.css';
 
-class Event extends Component{
-    render(){
-        return(
+class Event extends Component {
+    render() {
+        return (
             <div>
                 <div className="title">Events</div>
                 <div className="flex-container2">
-                    <div className="image"><img src="IMG_1789.jpg" alt="Ada Lovelace Hackathon"></img></div>
-                    <div className="image"><img src="IMG_1563.jpg" alt="Women in Computing"></img></div>
-                    <div className="image"><img src="IMG_1730.jpg" alt="Memory"></img></div>
+                    <img className="image" src={require("./IMG_1789.jpg")} alt="Ada Lovelace Hackathon" ></img>
+                    <img className="image" src={require("./IMG_1563.jpg")} alt="Women in Computing"></img>
+                    <img className="image" src={require("./IMG_1730.jpg")} alt="Memory"></img>
                 </div>
                 <div className="flex-container1">
                     <div className="textblock1">RSVP</div>
                     <div className="textblock2">RSVP</div>
-                    <div className="textblock3">More...</div>
+                    <div className="textblock3">Check this out</div>
                 </div>
             </div>
         )
