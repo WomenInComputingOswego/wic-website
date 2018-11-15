5a8facfccfadf601758b6996e62dc636780a408d
Tara O <togrady@oswego.edu>
2018-11-14 23:11:22 -0500

    Image/button style
    Styled image ang button. Not sure about the button color, and for the shape - I think there is color behind the text itself so the border radius looks weird.
    
    Button is not functional yet because I don't know where to tell it to go.

--- src/components/LearnMoreComponent/LearnMore.jsx
+++ src/components/LearnMoreComponent/LearnMore.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import wicCoffee from '../../assets/wicCoffee.jpg';
+import './LearnMore.css';
 
 class LearnMore extends Component{
     render(){
         return(
-            <div>
-                <img src={wicCoffee}/>
-                <div>
-                    <button>Learn More</button>
+            //header image and learn more link
+            <div class="container">
+                <img id="headerImage" src={wicCoffee} alt=""/>
+                <div id="buttonContainer">
+                    <button id="learnMoreButton">Learn More</button>
                 </div>
             </div>
         
