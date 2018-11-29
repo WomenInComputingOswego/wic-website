09f16cc8aa2d23f0e781db588454d03b48d88043
Tara O <togrady@oswego.edu>
2018-11-28 20:10:09 -0500

    outreach

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import { HashRouter, Route } from "react-router-dom";
-import HomePage from './components/HomePageComponent/HomePageComponent.jsx';
+import Outreach from './components/OutreachComponent/OutreachComponent.jsx';
 
 class Router extends Component {
     render() {
         return (
             <HashRouter>
                 <div style={{ height: "100%" }}>
-                    <Route exact path="/" component={HomePage} />
+                    <Route exact path="/" component={Outreach} />
                 </div>
             </HashRouter>
         )
---
+++ src/components/OutreachComponent/OutreachComponent.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import './OutreachComponent.css';
+
+class Event extends Component {
+    render() {
+        return (
+            <div>
+                <div className="title">Outreach</div>
+                <div className="flex-container2">
+                    
+                </div>
+                <div className="textblock">
+                    Check Out the Projects
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Event;
