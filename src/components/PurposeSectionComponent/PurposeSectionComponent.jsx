f56f4d9e3b6bc55b32e5da39633e42cac5a9d651
Arielle <aphilli4@oswego.edu>
2018-11-28 19:59:55 -0800

    PurposeSection
    Added the Css and Jsx component files

---
+++ src/components/PurposeSectionComponent/PurposeSectionComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class PurposeSection extends Component{
+    render(){
+        return(
+            <div>Home Page</div>
+        )
+    }
+}
+
+export default PurposeSection;
