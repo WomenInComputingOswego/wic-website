d491accf842574b975a9750e51c45bc0a70b405c
kchan2 <kayingchan0903@ymail.com>
2019-05-01 19:11:32 -0400

    Purpose Component Done
    Title div done. Images are showing. Captions are finished.

--- src/components/PurposeSectionComponent/PurposeSectionComponent.jsx
+++ src/components/PurposeSectionComponent/PurposeSectionComponent.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
+import './PurposeSectionComponent.css';
 
-class PurposeSection extends Component{
+class Purpose extends Component{
     render(){
         return(
-            <div>Home Page</div>
+            <div>
+            <div className="title">Purpose</div>
+            <div className="flex-container2">
+                    <img className="image" src={require("./download (2).jpg")} alt="Ada Lovelace Hackathon" ></img>
+                    <img className="image" src={require("./download.jpg")} alt="Women in Computing"></img>
+                    <img className="image" src={require("./download (1).jpg")} alt="Memory"></img>
+                </div>
+                <div className="flex-container1">
+                    <div className="textblock">Creating a support system</div>
+                    <div className="textblock">Providing members with a professional network of female engineers</div>
+                    <div className="textblock">Serving the community</div>
+                </div>
+            </div>
         )
     }
 }
 
-export default PurposeSection;
+export default Purpose;
