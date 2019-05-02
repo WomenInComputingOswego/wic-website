0f07350006eb2a0515e5c95fed3d0e433a7927ea
Tynise <35874024+Tynise@users.noreply.github.com>
2019-05-01 20:26:30 -0400

    working footer

--- src/components/FooterComponent/FooterComponent.jsx
+++ src/components/FooterComponent/FooterComponent.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import './FooterComponent.css';
 
-class Footer extends Component{
-    render(){
-        return(
-            <div className = "container"> Footer </div>
-            //make the div with an image in it and find out how to make it clickable 
-        );
+class Footer extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="footer">
+                    <img className="image" src={require("../../assets/email-wic.jpg")} alt="email"></img>
+                    <img className="image" src={require("../../assets/insta-wic.png")} alt="instagram"></img>
+                    <img className="image" src={require("../../assets/ozlogo-wic.png")} alt="lakerlife"></img>
+                </div>
+            </div>
+        )
     }
 }
 export default Footer;
